ba6bea7cfb9e6a4bbb1dcb48bcb8d54442725dba
Overlander <anuragnandal@hotmail.com>
2025-12-03 18:46:19 -0500

    Add operate function

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
-// console.log(add(1, 2))
-// console.log(subtract(1, 2))
-// console.log(multiply(5, 2))
-// console.log(divide(5, 2))
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+  }
+}
+
+// console.log(operate("+", 1, 2))
+// console.log(operate("-", 1, 2))
+// console.log(operate("*", 5, 2))
+// console.log(operate("/", 5, 2))
